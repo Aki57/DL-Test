e87991fda07d5c25109bd1206e9ae23f0196b45b
Aki57 <1152042601@qq.com>
2018-09-11 13:22:18 +0800

    reload-2

---
+++ 4-Extra/3 k nearest neighbor.py
@@ -0,0 +1,62 @@
+﻿import numpy as np
+import sys
+import os
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+###################################################################
+# Variables                                                       #
+# When launching project or scripts from Visual Studio,           #
+# input_dir and output_dir are passed as arguments automatically. #
+# Users could set them from the project setting page.             #
+###################################################################
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string("input_dir", ".", "Input directory where training dataset and meta data are saved")
+tf.app.flags.DEFINE_string("output_dir", ".", "Output directory where output such as logs are saved.")
+tf.app.flags.DEFINE_string("log_dir", ".", "Model directory where final model files are saved.")
+
+def main(_):
+    mnist = input_data.read_data_sets("/tmp/data", one_hot=True)
+
+    # In this example, we limit mnist data
+    Xtr, Ytr = mnist.train.next_batch(5000) # 5000 for training
+    Xte, Yte = mnist.train.next_batch(200) # 200 for testing
+
+    # tf Graph Input
+    xtr = tf.placeholder("float", [None, 784])
+    xte = tf.placeholder("float", [784])
+
+    # Nearest Neighbor calculation using L1 Distance
+    # Calculate L1 Distance
+    distance = tf.reduce_sum(tf.abs(tf.add(xtr, tf.negative(xte))), reduction_indices=1)
+
+    # Prediction: Get min distance index (Nearest neighbor)
+    pred = tf.argmin(distance, 0)
+    accuracy = 0.
+
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+
+        # Fit all training data
+        for i in range(len(Xte)):
+            # Get nearest neighbor
+            nn_index = sess.run(pred, feed_dict={xtr: Xtr, xte: Xte[i, :]})
+            # Get nearest neighbor class label and compare it to its true label
+            print("Test", i, "Prediction:", np.argmax(Ytr[nn_index]),
+                  "True Class:", np.argmax(Yte[i]))
+            # Calculate accuracy
+            if np.argmax(Ytr[nn_index]) == np.argmax(Yte[i]):
+                accuracy += 1./ len(Xte)
+
+        print("Done!")
+        print("Accuracy:", accuracy)
+        
+    exit(0)
+
+
+if __name__ == "__main__":
+    tf.app.run()
---
+++ 4-Extra/6 mlp networks.py
@@ -0,0 +1,102 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+###################################################################
+# Variables                                                       #
+# When launching project or scripts from Visual Studio,           #
+# input_dir and output_dir are passed as arguments automatically. #
+# Users could set them from the project setting page.             #
+###################################################################
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string("input_dir", ".", "Input directory where training dataset and meta data are saved")
+tf.app.flags.DEFINE_string("output_dir", ".", "Output directory where output such as logs are saved.")
+tf.app.flags.DEFINE_string("log_dir", ".", "Model directory where final model files are saved.")
+
+def main(_):
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+    # Parameters
+    learning_rate = 0.001
+    training_epochs = 15
+    batch_size = 100
+    display_step = 1
+
+    # Network Parameters
+    n_hidden_1 = 256 # 1st layer num features
+    n_hidden_2 = 256 # 2st layer num features
+    n_input = 784 # MNIST data input (img shape: 28*28)
+    n_classes = 10 # 1st layer num features
+
+    # tf Graph Input
+    x = tf.placeholder(tf.float32, [None, n_input])
+    y = tf.placeholder(tf.float32, [None, n_classes])
+
+    # Store layers weight & bias
+    weights = {
+        'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+        'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),        
+        'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+    }
+    biases = {
+        'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+        'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+
+    # Create model
+    def multilayer_perception(x, weights, biases):
+        # Hidden layer with RELU activation
+        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        layer_1 = tf.nn.relu(layer_1)
+        # Hidden layer with RELU activation
+        layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        layer_2 = tf.nn.relu(layer_2)
+        # Output layer with linear activation
+        out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+        return out_layer
+
+    # Construct model
+    pred = multilayer_perception(x, weights, biases)
+    
+    # Define loss and optimizer
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    # Initializing the variables
+    init = tf.global_variables_initializer()
+
+    # Launch the graph
+    with tf.Session() as sess:
+        sess.run(init)
+        
+        # Training cycle
+        for epoch in range(training_epochs):
+            avg_cost = 0.
+            total_batch = int(mnist.train.num_examples/batch_size)
+            # Loop over all batches
+            for i in range(total_batch):
+                batch_x, batch_y = mnist.train.next_batch(batch_size)
+                # Run optimization op (backprop) and cost op (to get loss value)
+                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                # Compute average loss
+                avg_cost += c / total_batch
+            # Display logs per epoch step
+            if epoch % display_step == 0:
+                print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+        print("Optimization Finished!")
+
+        # Test model
+        correct_prediction = tf.equal(tf.arg_max(pred, 1), tf.arg_max(y, 1))
+        # Calculate accuracy
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        print("Accuracy:", accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+        
+    exit(0)
+    
+if __name__ == "__main__":
+    tf.app.run()
