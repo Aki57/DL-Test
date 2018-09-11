645dc6fdbeec6495f2f160285817ca24fc042aab
Aki57 <1152042601@qq.com>
2018-09-11 13:11:17 +0800

    reload-1

--- 0-Basic/relu.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-a = tf.constant([1,-2,0,4,-5,6])
-b = tf.nn.relu(a)
-with tf.Session() as sess:
-    print (sess.run(b))
--- 0-Basic/truncated_normal.py
+++
@@ -1,3 +0,0 @@
-import tensorflow as tf
-initial = tf.truncated_normal(shape=[3,3], mean=0, stddev=1)
-print(tf.Session().run(initial))
